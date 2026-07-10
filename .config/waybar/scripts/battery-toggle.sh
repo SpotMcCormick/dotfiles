55a53e4c96a3d79fb35aa551c5349d6322169eb6
jeremymccormick <spotmccormick@gmail.com>
2026-07-10 10:45:01 -0500

    weekly arch update

--- .config/waybar/scripts/battery-toggle.sh
+++ .config/waybar/scripts/battery-toggle.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-BAT="BAT0"
+CURRENT=$(powerprofilesctl get)
 
-END=$(cat /sys/class/power_supply/$BAT/charge_control_end_threshold)
-
-if [ "$END" = "100" ]; then
-    echo 75 | sudo tee /sys/class/power_supply/$BAT/charge_control_start_threshold
-    echo 80 | sudo tee /sys/class/power_supply/$BAT/charge_control_end_threshold
-    notify-send "Battery Mode" "Power saver: 80%"
+if [ "$CURRENT" = "performance" ]; then
+    powerprofilesctl set power-saver
+    notify-send "Power Profile" "Battery saver mode"
 else
-    echo 90 | sudo tee /sys/class/power_supply/$BAT/charge_control_start_threshold
-    echo 100 | sudo tee /sys/class/power_supply/$BAT/charge_control_end_threshold
-    notify-send "Battery Mode" "Performance: 100%"
-fi
-	
+    powerprofilesctl set performance
+    notify-send "Power Profile" "Performance mode"
+fi	
